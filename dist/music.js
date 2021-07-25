2a12abc954f0240635b8a730404d1b5da6fa1c2e
litmist <18267698209@163.com>
2021-07-26 02:08:35 +0800

    Site updated: 2021-07-26 02:08:26

---
+++ dist/music.js
@@ -0,0 +1,21 @@
+/*const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+    {
+        name: 'A Rush Of Blood To The Head',
+        artist: 'Coldplay',
+        url: 'http://music.163.com/song/media/outer/url?id=25408336.mp3',
+        cover: '/images/ar.jfif',
+      },
+    ]
+});*/
+
+
+//写在_layout.swig里下面的
+//  <link rel="stylesheet" href="/dist/APlayer.min.css">
+//  <div id="aplayer"></div>
+//  <script type="text/javascript" src="/dist/APlayer.min.js"></script>
+//  <script type="text/javascript" src="/dist/music.js"></script>
+
